b631c95b8bcf1289f33b532bee13215bdf98a4db
nopriterbang <novalpriya@gmail.com>
2024-06-14 17:03:53 +0700

    membuat perulangan untuk mencari elemen

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -56,5 +56,21 @@ public:
 
 	
 	}
+
+	void search(string element, Node*& parent, Node* currentNode)
+	{
+		// This function searches the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+
+		while ((currentNode != NULL) && (currentNode->info != element)) {
+			parent = currentNode;
+			if (element < currentNode->info) {
+				currentNode = currentNode->leftchild;
+			}
+			else {
+				currentNode = currentNode->rightchild;
+			}
+	}
 };
 
