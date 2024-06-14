c0cf2f44806c0b406f3ee24337c3d874e58553a9
nopriterbang <novalpriya@gmail.com>
2024-06-14 17:26:12 +0700

    menambahkan void posorder untuk melakukan traversal postorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -95,7 +95,25 @@ public:
 			cout << "Tree is empty" << endl;
 			return;
 		}
+		if (ptr != NULL) {
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
 	
+	}
+
+	void postorder(Node* ptr)
+	{
+		if (ROOT == NULL) {
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			postorder(ptr->leftchild); 
+			postorder(ptr->rightchild); 
+			cout << ptr->info << " "; 
+		}
 	}
 
 
